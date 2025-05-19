db1cb1b8a1411b94d56e8a78b70f2ff742ed2530
NANA617 <2633404777@qq.com>
2025-05-19 19:21:07 +0800

    fourth commit

--- src/main/java/com/offcn/git/HelloGit.java
+++ src/main/java/com/offcn/git/HelloGit.java
@@ -7,4 +7,9 @@ package com.offcn.git;
  * @Version:1.0
  */
 public class HelloGit {
+  public static void main(String[]args){
+    System.out.println("hello Git!");
+    System.out.println("hello Hot-Fix!");
+    System.out.println("hello GitHub!");
+  }
 }
